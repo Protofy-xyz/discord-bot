10f1b935559ddb981a5a765ae27beade626b91b1
miquel <miquel@protofy.xyz>
2025-06-03 12:00:11 +0200

    Adds connect bot and create and handle command automations

--- packages/app/apis/index.ts
+++ packages/app/apis/index.ts
@@ -1,6 +1,11 @@
 import {Protofy} from 'protobase'
+import discord_connect_botApi from "./discord_connect_bot";
+import discord_command_helloApi from "./discord_command_hello";
 
-const autoApis = Protofy("apis", {})
+const autoApis = Protofy("apis", {
+    discord_connect_bot: discord_connect_botApi,
+    discord_command_hello: discord_command_helloApi
+})
 
 export default (app, context) => {
     Object.keys(autoApis).forEach((k) => {
